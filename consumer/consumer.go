62b8a8ec90743c8de2204e47d00a9cfe3fc2c849
Daniel Kogan <daniel.kogan@stonybrook.edu>
2024-02-20 11:56:52 -0500

    open a new consumer service server, so that the producer can reach out to me

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"net"
 	"time"
 
 	pb "github.com/daminals/cse416-init-repo-union-1/peernode"
@@ -15,6 +17,10 @@ var (
 	addr = flag.String("addr", "localhost:50051", "the address to connect to")
 )
 
+type server struct {
+	pb.UnimplementedConsumerServiceServer
+}
+
 func main() {
 	flag.Parse()
 	// Set up a connection to the server.
@@ -25,6 +31,18 @@ func main() {
 	defer conn.Close()
 	c := pb.NewMarketServiceClient(conn)
 
+	// parse the port from the addr string
+	_, port, err := net.SplitHostPort(*addr)
+	if err != nil {
+		log.Fatalf("Failed to parse port: %v", err)
+	}
+	// convert the port string to an int
+	portInt, err := net.LookupPort("tcp", port)
+
+	if err != nil {
+		log.Fatalf("Failed to convert port to int: %v", err)
+	}
+
 	// Contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
@@ -33,4 +51,17 @@ func main() {
 		log.Fatalf("could not add file request: %v", err)
 	}
 	log.Printf("Made file request!")
+
+	// now i open a grpc connection for the producer to reach out to me
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", portInt))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterConsumerServiceServer(s, &server{})
+	log.Printf("Market Server listening on port %d...\n", portInt)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
